2e03291cee1d72e04469ff6ca81ef7b82e764e80
Dale Gilliam <26942296+data-modeler@users.noreply.github.com>
2020-02-06 18:05:28 -0500

    Uncomment lines

--- src/models/rnnsurv/create_model.py
+++ src/models/rnnsurv/create_model.py
@@ -25,51 +25,51 @@ def create_model(n_features,
     '''Creates the RNN-SURV based on hyperparameters.
     '''
     # define the model architecture
-    # x_in = Input(batch_shape=(None, max_length, n_features), name='x_in')
-    # loss_mask = Input(batch_shape=(None, max_length), name='loss_mask')
-    # flip_i = Input(batch_shape=(None, None), name='flip_i')
+    x_in = Input(batch_shape=(None, max_length, n_features), name='x_in')
+    loss_mask = Input(batch_shape=(None, max_length), name='loss_mask')
+    flip_i = Input(batch_shape=(None, None), name='flip_i')
 
-    # dense_layers = Sequential(name='denses')
-    # for i, d_size in enumerate(dense_sizes):
-    #     if i == 0:
-    #         dense_layers.add(
-    #             TimeDistributed(
-    #                 Dense(d_size, activation='relu'),
-    #                       input_shape=(max_length, n_features)))
-    #         dense_layers.add(Dropout(dropout_prob))
-    #     else:
-    #         dense_layers.add(
-    #             TimeDistributed(
-    #                 Dense(d_size, activation='relu'),
-    #                       input_shape=(max_length, dense_sizes[i-1])))
-    #         dense_layers.add(Dropout(dropout_prob))
+    dense_layers = Sequential(name='denses')
+    for i, d_size in enumerate(dense_sizes):
+        if i == 0:
+            dense_layers.add(
+                TimeDistributed(
+                    Dense(d_size, activation='relu'),
+                          input_shape=(max_length, n_features)))
+            dense_layers.add(Dropout(dropout_prob))
+        else:
+            dense_layers.add(
+                TimeDistributed(
+                    Dense(d_size, activation='relu'),
+                          input_shape=(max_length, dense_sizes[i-1])))
+            dense_layers.add(Dropout(dropout_prob))
 
-    # layers = Sequential(name='y_hat')
-    # for l_size in lstm_sizes:
-    #     layers.add(LSTM(l_size, return_sequences=True))
+    layers = Sequential(name='y_hat')
+    for l_size in lstm_sizes:
+        layers.add(LSTM(l_size, return_sequences=True))
 
-    # layers.add(Dropout(dropout_prob))
-    # layers.add(TimeDistributed(Dense(1, activation='sigmoid')))
-    # layers.add(Flatten())
+    layers.add(Dropout(dropout_prob))
+    layers.add(TimeDistributed(Dense(1, activation='sigmoid')))
+    layers.add(Flatten())
 
-    # calculate_r = Dense(1, activation='linear', name='r_out')
+    calculate_r = Dense(1, activation='linear', name='r_out')
 
-    # # connect the network layers
-    # denses = dense_layers(x_in)
-    # y_hat = layers(denses)
-    # r_out = calculate_r(y_hat)
+    # connect the network layers
+    denses = dense_layers(x_in)
+    y_hat = layers(denses)
+    r_out = calculate_r(y_hat)
 
-    # losses = {
-    #         "y_hat": loss1(pad_token),
-    #         "r_out": loss2(loss_mask, pad_token, flip_i),
-    # }
+    losses = {
+            "y_hat": loss1(pad_token),
+            "r_out": loss2(loss_mask, pad_token, flip_i),
+    }
 
-    # model = Model([x_in, loss_mask, flip_i], [r_out, y_hat])
-    # model.compile(optimizer=optimizer, loss=losses, loss_weights=loss_weights,
-    #               experimental_run_tf_function=False)
-    # # the argument `experimental_run_tf_function=False` was added per this issue:
-    # # https://github.com/tensorflow/probability/issues/519
-    # # this was throwing the same error
-    # model.summary()
+    model = Model([x_in, loss_mask, flip_i], [r_out, y_hat])
+    model.compile(optimizer=optimizer, loss=losses, loss_weights=loss_weights,
+                  experimental_run_tf_function=False)
+    # the argument `experimental_run_tf_function=False` was added per this issue:
+    # https://github.com/tensorflow/probability/issues/519
+    # this was throwing the same error
+    model.summary()
     
     return model
