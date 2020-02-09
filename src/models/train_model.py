76083f8360ef996a05fca591a5827b4f622596e4
Dale Gilliam <dale@gilliam.biz>
2020-02-08 19:10:47 -0500

    Add model saving

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,14 +1,16 @@
 '''
+    TODO:
     4. calculate C-Index from final predictions
     5. set up hyperparam tuning
 '''
 
+import os
 import numpy as np
 import pandas as pd
 
 from rnnsurv import get_data, DataGenerator, create_model
 
-
+print('Getting Data...')
 XT, YT = get_data()
 
 N_FEATURES = XT.shape[1] - 1 
@@ -23,18 +25,30 @@ MODEL_PARAMS = {
     'loss_weights': {"y_hat": 1.0, "r_out": 1.0}
 }
 
+print('Creating model...')
 MODEL = create_model(N_FEATURES, **MODEL_PARAMS)
 
 PARAMS = {
     'max_timesteps': MODEL_PARAMS['max_length'],
     'padding_token': MODEL_PARAMS['pad_token'],
-    'max_batch_size': 64,
+    'max_batch_size': 128,
     'min_batch_size': 32,
     'shuffle': True,
 }
 
 TRAIN_GENERATOR = DataGenerator(XT, YT, **PARAMS)
 
-MODEL.fit(TRAIN_GENERATOR, epochs=9)
+print('Training model...')
+MODEL.fit_generator(TRAIN_GENERATOR, epochs=9)
 
+MYDIR = '../../models'
+
+# serialize model to JSON
+MODEL_JSON = MODEL.to_json()
+with open(os.path.join(MYDIR, "model.json"), "w") as json_file:
+    json_file.write(MODEL_JSON)
+
+# serialize weights to HDF5
+MODEL.save_weights(os.path.join(MYDIR, "model.h5"))
+print("Saved model to disk")
 
